1b7eabf8cc3aed1bebf8ee1c9a9c41bc82a92f46
Jeffer Zhang <zhangjeffer@gmail.com>
2021-11-26 20:35:23 -0500

    Initial commit

---
+++ src/rent_calc.py
@@ -0,0 +1,45 @@
+DOWN_PAYMENT = 20
+INTEREST_RATE = 3.1
+LOAN_TERM = 30
+
+'''
+Numbers avaliable from Zillow listing 
+    - Purchase Price
+    - Monthly Rent 
+    - Property Tax
+'''
+
+#Pull from Zillow
+def get_purchase_price():
+    return 
+
+
+#Pull from NerdWallet
+def get_closing_cost():
+    return
+
+
+#Pull from Zillow
+def get_property_tax():
+    return
+
+
+#Pull from NerdWallet
+def get_insurance_cost():
+    return 
+
+
+def get_monthly_expenses():
+    return
+
+
+def get_monthly_cash_flow():
+    return
+
+
+def main():
+    print("Welcome to Rentify")
+
+
+if __name__ == "__main__":
+    main()
